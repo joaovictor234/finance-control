c1230d51ee8ea8a767a026333528041958df36a6
João Victor Fernandes <joaovictorfernandespinto@gmail.com>
2023-11-08 15:22:25 -0300

    update categories list

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -2,106 +2,124 @@ export const CATEGORIES_INCOME = [
   {
     id: 1,
     name: "Salário",
-    color: "#FFEE4A",
+    color: "#45BF55",
     icon: "cash",
   },
   {
     id: 2,
     name: "Rendimentos",
-    color: "#F2A30F",
-    icon: "trending-up"
+    color: "#003FFF",
+    icon: "trending-up",
   },
   {
     id: 3,
-    name: "Prêmios",
-    color: "#F2780C",
-    icon: "ios-trophy"
+    name: "Bônus",
+    color: "#FF5E35",
+    icon: "gift",
   },
   {
     id: 4,
-    name: "Bônus",
-    color: "#FF5E35",
-    icon: "gift"
-  },
-  {
-    id: 5,
     name: "Outros",
     color: "#7F1C19",
-    icon: "ellipsis-vertical"
-  }
+    icon: "ellipsis-vertical",
+  },
 ];
 
 export const CATEGORIES_EXPENSE = [
   {
     id: 1,
     name: "Casa",
-    color: "#FFEE4A",
-    icon: "home"
+    color: "#ADB0D9",
+    icon: "home",
   },
   {
     id: 2,
-    name: "Educação",
-    color: "#F2A30F",
-    icon: "book"
+    name: "Alimentação",
+    color: "#F22D20",
+    icon: "nutrition",
   },
   {
     id: 3,
-    name: "Eletrônicos",
-    color: "#F2780C",
-    icon: "desktop"
+    name: "Água",
+    color: "#00C7E8",
+    icon: "water",
   },
   {
     id: 4,
-    name: "Investimentos",
-    color: "#FF5E35",
-    icon: "trending-up"
+    name: "Luz",
+    color: "#F2CC0F",
+    icon: "bulb",
   },
   {
     id: 5,
-    name: "Lazer",
-    color: "#7F1C19",
-    icon: "game-controller"
+    name: "Educação",
+    color: "#87FCFF",
+    icon: "book",
   },
   {
     id: 6,
-    name: "Necessidade",
-    color: "#E5322C",
-    icon: "wallet"
+    name: "Eletrônicos",
+    color: "#00C7E8",
+    icon: "desktop",
   },
   {
     id: 7,
-    name: "Saúde",
-    color: "#EF4444",
-    icon: "medkit"
+    name: "Lazer",
+    color: "#F2CC0F",
+    icon: "game-controller",
   },
   {
     id: 8,
-    name: "Animal",
-    color: "#ADB0D9",
-    icon: "paw"
+    name: "Necessidade",
+    color: "#FF5E35",
+    icon: "wallet",
   },
   {
     id: 9,
-    name: "Transporte",
-    color: "#ADB0D9",
-    icon: "md-car"
+    name: "Saúde",
+    color: "#E5322C",
+    icon: "medkit",
   },
   {
     id: 10,
-    name: "Vestuário",
-    color: "#4A1259",
-    icon: "shirt"
+    name: "Investimentos",
+    color: "#003FFF",
+    icon: "trending-up",
   },
   {
     id: 11,
-    name: "Viagem",
-    color: "#200726",
-    icon: "plane"
+    name: "Animal",
+    color: "#732C02",
+    icon: "paw",
   },
   {
     id: 12,
+    name: "Transporte",
+    color: "#8FA1A6",
+    icon: "md-car",
+  },
+  {
+    id: 13,
+    name: "Vestuário",
+    color: "#4A1259",
+    icon: "shirt",
+  },
+  {
+    id: 14,
+    name: "Viagem",
+    color: "#F20544",
+    icon: "plane",
+  },
+  {
+    id: 15,
+    name: "Telefone",
+    color: "#36175E",
+    icon: "phone-portrait",
+  },
+  {
+    id: 16,
     name: "Outros",
     color: "#00C7E8",
-    icon: "ellipsis-vertical"
-  }
-]
+    icon: "ellipsis-vertical",
+  },
+];
